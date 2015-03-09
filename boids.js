93d45affac0dec9ca9f25ee9b1e8b524d64d391e
charlierproctor <charlie.proctor@yahoo.com>
2015-03-09 00:11:07 -0400

    adding ticker code to boids.js

--- boids.js
+++ boids.js
@@ -1,1 +1,17 @@
-var ticker = require("ticker")
+window.onload = function(){
+var ticker = require('ticker');
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2d');
+var x = 0, y = 0;
+ 
+ticker(window, 60).on('tick', function() {
+  x += Math.round(Math.random()*2-1)*10
+  y += Math.round(Math.random()*2-1)*10
+}).on('draw', function() {
+  ctx.fillStyle = 'black'
+  ctx.fillRect(0, 0, canvas.width, canvas.height)
+  ctx.fillStyle = 'white'
+  ctx.fillRect(x, y, 10, 10)
+})
+}
+
