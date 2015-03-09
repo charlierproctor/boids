6984d99f01964a085e26b55f96a7c86ecdadf34e
charlierproctor <charlie.proctor@yahoo.com>
2015-03-09 15:20:10 -0400

    moving coords to coords.js

--- boids.js
+++ boids.js
@@ -1,6 +1,7 @@
-var ticker, canvas, ctx, theBoids;
+var ticker, canvas, ctx, theBoids, coords;
 window.onload = function(){
 	ticker = require('ticker');
+	coords = require('./coords');
 	
 	canvas = document.getElementById('canvas');
 	canvas.width = window.innerWidth;
@@ -37,6 +38,7 @@ function boids(num, opts){
 	this.arr = [];
 	this.createBoids(opts);
 }
+
 boids.prototype.createBoids = function(opts){
 	var id = 0;
 	for (var i = 0; i < this.num; i++) {
@@ -156,48 +158,4 @@ boid.prototype.move = function(){
 // force angles between -Math.PI and Math.PI
 function rangify(angle){
 	return (angle + Math.PI) % (2 * Math.PI) - Math.PI;
-}
-
-function coords(x,y){
-	this.x = x;
-	this.y = y;
-}
-
-coords.prototype.add = function(coords){
-	this.x += coords.x;
-	this.y += coords.y;
-	return this;
-}
-coords.prototype.rotate = function(radians){
-	var x = this.x * Math.cos(radians) - this.y * Math.sin(radians);
-	var y = this.x * Math.sin(radians) + this.y * Math.cos(radians);
-	this.x = x;
-	this.y = y;
-}
-coords.prototype.angleTo = function(coords){
-	var y = coords.y - this.y, x = coords.x - this.x;
-	var res = Math.atan(y / x);
-	if (x < 0) {
-		if (y > 0){
-			res += Math.PI;
-		} else {
-			res -= Math.PI;
-		}
-	}
-	return res;
-}
-coords.prototype.checkBounds = function(){
-	var height = window.innerHeight, width = window.innerWidth;
-	if (this.x < 0){
-		this.x += width;
-	}
-	if (this.y < 0){
-		this.y += height
-	}
-	if (this.x > width){
-		this.x -= width
-	}
-	if (this.y > height){
-		this.y -= height
-	}
 }
---
+++ coords.js
@@ -0,0 +1,45 @@
+function coords(x,y){
+	this.x = x;
+	this.y = y;
+}
+
+coords.prototype.add = function(coords){
+	this.x += coords.x;
+	this.y += coords.y;
+	return this;
+}
+coords.prototype.rotate = function(radians){
+	var x = this.x * Math.cos(radians) - this.y * Math.sin(radians);
+	var y = this.x * Math.sin(radians) + this.y * Math.cos(radians);
+	this.x = x;
+	this.y = y;
+}
+coords.prototype.angleTo = function(coords){
+	var y = coords.y - this.y, x = coords.x - this.x;
+	var res = Math.atan(y / x);
+	if (x < 0) {
+		if (y > 0){
+			res += Math.PI;
+		} else {
+			res -= Math.PI;
+		}
+	}
+	return res;
+}
+coords.prototype.checkBounds = function(){
+	var height = window.innerHeight, width = window.innerWidth;
+	if (this.x < 0){
+		this.x += width;
+	}
+	if (this.y < 0){
+		this.y += height
+	}
+	if (this.x > width){
+		this.x -= width
+	}
+	if (this.y > height){
+		this.y -= height
+	}
+}
+
+module.exports = coords;
