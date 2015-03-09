59609dd2f898fbfafe57b0c2ab1ab590bd02f7a1
charlierproctor <charlie.proctor@yahoo.com>
2015-03-09 01:39:48 -0400

    can now rotate triangle!!

--- boids.js
+++ boids.js
@@ -12,25 +12,35 @@ window.onload = function(){
 
 var x;
 function tick(){
-	x = new boid(100,200);
+	x = new boid(new coords(100,200),Math.PI / 2);
 }
 
 function draw(){
 	x.draw();
 }
 
-function boid(posx,posy){
-	this.posx = posx;
-	this.posy = posy;
+function boid(coords,heading){
+	this.coords = coords;
+	this.heading = heading;
 }
 
 var base = 10, height = 20;
 boid.prototype.draw = function(){
+	var a = new coords(0, height / 2);
+	a.rotate(this.heading);
+	a.add(this.coords);
+	var b = new coords(base / 2, -height / 2);
+	b.rotate(this.heading);
+	b.add(this.coords);
+	var c = new coords(-base / 2, -height / 2);
+	c.rotate(this.heading);
+	c.add(this.coords);
 
+	// console.log(a,b,c);
 	ctx.beginPath();
-	ctx.moveTo(this.posx,this.posy);
-	ctx.lineTo(this.posx+b,this.posy+a);
-	ctx.lineTo(this.posx+d,this.posy+c);
+	ctx.moveTo(a.x,a.y);
+	ctx.lineTo(b.x,b.y);
+	ctx.lineTo(c.x,c.y);
 	ctx.closePath();
 	ctx.stroke();
 };
@@ -40,6 +50,10 @@ function coords(x,y){
 	this.y = y;
 }
 
+coords.prototype.add = function(coords){
+	this.x += coords.x;
+	this.y += coords.y;
+}
 coords.prototype.rotate = function(radians){
 	var x = this.x * Math.cos(radians) - this.y * Math.sin(radians);
 	var y = this.x * Math.sin(radians) + this.y * Math.cos(radians);
