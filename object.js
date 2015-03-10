b836428fc81806f253d0ad230f58e5295d9f54ce
charlierproctor <charlie.proctor@yahoo.com>
2015-03-09 21:00:55 -0400

    coloring attractors / obstacles

--- object.js
+++ object.js
@@ -11,6 +11,17 @@ object.prototype.draw = function(ctx){
 	ctx.beginPath();
 	ctx.ellipse(this.pos.x, this.pos.y, this.shape.radius, this.shape.radius, 0, 0, 2 * Math.PI);
 	ctx.stroke();
+	var red = 255, green = 255;
+	if (this.strength < 0) {
+		green -= Math.abs(this.strength) * 255;
+	} else {
+		red -= Math.abs(this.strength) * 255;
+	}
+	ctx.fillStyle = "rgb(" + Math.round(red) + "," + Math.round(green) + ",0)";
+	ctx.fill();
+	ctx.stroke()
+	ctx.fillStyle = "black";
+
 };
 
 module.exports = object;
